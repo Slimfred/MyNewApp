975810e9f7a855a18c4037369fa71aaee40c93bd
Slimfred <slimfred37@gmail.com>
2017-03-21 13:19:23 -0400

    6.6 change redirect to payments_create thank you page

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -35,7 +35,8 @@ class PaymentsController < ApplicationController
       flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
     end
 
-    redirect_to product_path(@product), notice: "Your payment has been processed!"
+    redirect_to payments_create_path
+    # redirect_to product_path(@product), notice: "Your payment has been processed!"
 
   end
 end
